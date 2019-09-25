ffc6a17c0cdd13a09d84621846b52e7e5124c85a
elifoster <elifosterwy@gmail.com>
2019-09-25 15:57:41 -0700

    :gem: Add new main page section edits for official wikis

--- lib/spinup/action_modules/pagetitles.rb
+++ lib/spinup/action_modules/pagetitles.rb
@@ -4,10 +4,16 @@ class Pagetitles < ActionModule
   end
 
   SUMMARY = 'Setting to official.'
-  SITENAME = 'Official {{SITENAME}}'
+  PAGETITLE = 'Official {{SITENAME}}'
 
   def execute
-    @mw.create_page('MediaWiki:Pagetitle', "$1 - #{SITENAME}", summary: SUMMARY)
-    @mw.create_page('MediaWiki:Pagetitle-view-mainpage', SITENAME, summary: SUMMARY)
+    @mw.create_page('MediaWiki:Pagetitle', "$1 - #{PAGETITLE}", summary: SUMMARY)
+    @mw.create_page('MediaWiki:Pagetitle-view-mainpage', PAGETITLE, summary: SUMMARY)
+
+    section1_text = @mw.get_text("#{SITENAME}/Section 1")
+    @mw.edit("#{SITENAME}/Section 1", section1_text.gsub('Welcome to the {{Gamename}} Wiki', 'Welcome to the Official {{Gamename}} Wiki'), summary: SUMMARY)
+
+    sectiontop_text = @mw.get_text("#{SITENAME}/Top section")
+    @mw.edit("#{SITENAME}/Top section", sectiontop_text.gsub("Welcome to ''{{Gamename}}'' Wiki", 'Welcome to the Official {{Gamename}} Wiki'), summary: SUMMARY)
   end
 end
