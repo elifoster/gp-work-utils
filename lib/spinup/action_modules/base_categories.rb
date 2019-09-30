b6856b2e72ee8588c959564c386278d133b981c5
elifoster <elifosterwy@gmail.com>
2019-09-30 14:13:04 -0700

    Update basecategories spinup module for new SEO standard

--- lib/spinup/action_modules/base_categories.rb
+++ lib/spinup/action_modules/base_categories.rb
@@ -1,7 +1,6 @@
 class BaseCategories < ActionModule
   def execute
-    @mw.create_page("Category:#{GAMENAME}", '[[Category:Contents]]')
-    @mw.create_page("Category:#{SITENAME}", '[[Category:Contents]]')
-    @mw.create_page('Category:Contents', 'This is the root category.')
+    @mw.create_page("Category:#{SITENAME}", 'This is the root category.')
+    @mw.create_page("Category:#{GAMENAME}", "[[Category:#{SITENAME}]]")
   end
 end
