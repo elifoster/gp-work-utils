8b70194f0c5c75573e936f699278c07881b20fc7
elifoster <elifosterwy@gmail.com>
2019-10-11 12:23:28 -0700

    :gem: Add new main page talk page spinup action module

---
+++ lib/spinup/action_modules/main_page_talk.rb
@@ -0,0 +1,6 @@
+class MainPageTalk < ActionModule
+  def execute
+    cp_talk = @mw.get_text('Project talk:Community portal')
+    @mw.create_page("Talk:#{SITENAME}", cp_talk, summary: 'Creating base talk page to prevent red links on the main page.')
+  end
+end
