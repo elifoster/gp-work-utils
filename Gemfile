50052fa50bcb76e70ed21346bc0ecf28f7a3d24d
elifoster <elifosterwy@gmail.com>
2020-05-06 13:17:23 -0700

    :arrow_up: Update MediaWiki-Butt

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@ ruby '~> 2.6'
 source 'https://rubygems.org'
 
 gem 'dotenv', '~> 2.2'
-gem 'mediawiki-butt', '~> 3.0'
+gem 'mediawiki-butt', '~> 4.0'
 gem 'nokogiri', '~> 1.10'
 gem 'rainbow', '~> 3.0'
 gem 'w3c_validators', '~> 1.3'
--- lib/butt_extensions.rb
+++ lib/butt_extensions.rb
@@ -2,24 +2,6 @@ require 'mediawiki/butt'
 
 module ButtExtensions
   refine MediaWiki::Butt do
-    # An incomplete extension function to patrol revisions.
-    # @param opts [Hash<Symbol, String>] Options param
-    # @option :rcid Recentchanges ID to patrol
-    # @option :revid Revision ID to patrol
-    # @option :tags Change tags to apply to the entry in the patrol log.
-    # @see https://www.mediawiki.org/wiki/API:Patrol Patrol API documentation
-    # @return see #post
-    def patrol(opts = {})
-      params = {
-        action: 'patrol',
-        token: get_token('patrol')
-      }
-      params[:rcid] = opts[:rcid] if opts[:rcid]
-      params[:revid] = opts[:revid] if opts[:revid]
-      params[:tags] = opts[:tags] if opts[:tags]
-      post(params)
-    end
-
     # An extension function to parse the provided string into html
     # @param str [String] The string to parse
     # @return [String] The returned parsed wikitext
